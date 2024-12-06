8995ee4a355da5dd96c25be9dbf731b5b1851ac1
habibulla552272 <mdhabibulla146425@gmail.com>
2024-12-06 19:53:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// let x = prompt("text here", "hey boss");
+// console.log(x);
+
+// let y =confirm('are you boss ?');
+// console.log(y);
+
+//convert string
+// let value = true;
+// alert(typeof value);
+
+// value = String(value);
+// alert(typeof value);
+
+// convert number
+// let str = "111";
+// alert(typeof str);
+
+// let num = Number(str);
+// alert(typeof num);
+// alert(Number("145"));
+// alert(Number("333s"));
+// alert(Number(true));
+// alert(Number(false));
+
+//convert boolean
+
+// alert(Boolean(1));
+// alert(Boolean(0));
+// alert(Boolean("hello"));
+// alert(Boolean(""));
+
+// alert(Boolean(' 0'))
+
+//operators
+
+// alert(4 ** 2)
+
+// alert(8 ** (1/3));
+
+// alert("3"+3)
+// alert(2+3+"3")
+// alert('3'+3+3)
+// alert(6 -'4');
+
+let counter = 1;
+// let a=counter++
+// console.log(a);
+//  ++counter;
+//  console.log(counter);
+let c= ++counter
+ console.log(c);
+ 
+//comma
+
+// let a = (1+2,2*3);
+// console.log(a);
+
+
+
